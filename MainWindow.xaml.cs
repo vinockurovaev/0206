9aff0169406f031424c23cfe2e8f950fe6c2fecc
Елена Винокурова <vinockurova.el@yandex.ru>
2022-06-02 17:00:09 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace СтудентП31
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Model.СтудентП31Entities _context = new Model.СтудентП31Entities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DGCtud.ItemsSource = _context.Студент.ToList();
+        }
+    }
+}
---
+++ Model/Студент1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СтудентП31.Model
+{
+    public partial class Студент
+    {
+        public int Age
+        {
+            get
+            {
+                if (ДатаРождения == null)
+                {
+                    return 0;
+                }
+                var now = DateTime.Now;
+                var diff = now - ДатаРождения;
+                var years = diff.Value.Days / 365;
+
+                return (int)years;
+            }
+        }
+        public string BackColorAge
+        {
+            get
+            {
+                if (Age >= 17)
+                    return "#7fff00";
+                else
+                    return "#eed4fe";
+
+            }
+        }
+
+
+
+    }
+}
